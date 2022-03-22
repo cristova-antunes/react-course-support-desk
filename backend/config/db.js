f5635577f06ee9e2721bbafc85e4d4034aaaacfd
Cristóvão Antunes <cristovao.av.antunes@gmail.com>
2022-03-22 21:25:59 +0000

    fix db js

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
 
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
   } catch (e) {
-    console.log(`Error: ${error.message}`.red.underline.bold)
+    console.log(`Error: ${e.message}`.red.underline.bold)
 
     process.exit(1)
   }
